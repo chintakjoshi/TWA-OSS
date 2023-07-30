14193caa79b9a006c0988d3690369f2cf7c1603c
sai chetan veeragandham <saichetan.veeragandham@slu.edu>
2023-07-29 22:17:42 -0500

    fetching loaded seeker files

--- twa/TWA.py
+++ twa/TWA.py
@@ -56,9 +56,23 @@ def load_employer(*args):
     master2.mainloop()
     master.withdraw()
     master.destroy()
-    
 
-
+def jobseeker_details(master2):
+    cs.execute("SELECT FIRST_NAME,LAST_NAME FROM TWA.SCHEMA_TWA.JOBSEEKER")
+    label2 = Label(master2, text="", font=("Arial", 12))
+    label2.pack()
+    label2.config(text=cs.fetchall())        
+def load_jobseeker(*args):
+    master3 = Tk()
+    master3.geometry("400x300")
+    master3.title("jobseeker Details")
+    button5 = Button(master3, text="click to see loaded jobseeker Details", 
+    bg="blue", fg="white", padx=10, pady=5, font=("Arial", 10, "bold"),
+    command = lambda : jobseeker_details(master3))
+    button5.pack()
+    master3.mainloop()
+    master.withdraw()
+    master.destroy()  
 def fetching_details(*args):
     master1 = Tk()
     master1.geometry("400x300")
@@ -67,10 +81,10 @@ def fetching_details(*args):
     label_JsName = Label(master1, text="Please Select Job Seeker Name!", bg="lightgray", pady=10, font=("Arial", 12))
     label_JsName.pack()
 
-    label1 = Label(master1, text="", bg="lightgray", font=("Arial", 12))
+    label1 = Label(master1, text="", font=("Arial", 12))
     label1.pack()
 
-    label2 = Label(master1, text="", bg="lightgray", font=("Arial", 12))
+    label2 = Label(master1, text="", font=("Arial", 12))
     label2.pack()
 
     style = ttk.Style()
@@ -78,6 +92,7 @@ def fetching_details(*args):
 
     variable = StringVar(master1)
     variable.set("select jobseeker")  # default value
+    # variable.trace("w")
 
     def update_labels(*args):
         selected_option = variable.get()
@@ -91,7 +106,6 @@ def fetching_details(*args):
 
         query = "SELECT JOB_POSITIONS FROM  TWA.SCHEMA_TWA.JOBDETAILS WHERE OFFENSE_EXEMPTIONS NOT LIKE %s "
         cs.execute(query,'%'+string_cr_record+'%')
-        query_select = "select * from TWA.SCHEMA_TWA.JOBDETAILS"
         JobDetails_result = cs.fetchone()
         if selected_option != "select jobseeker":
             label1.config(text="Selected Job Seeker: " + selected_option)
@@ -112,16 +126,24 @@ def fetching_details(*args):
     master.destroy()
 
 
+
 master = Tk()
-master.geometry("400x300")
+master.geometry("400x300")  # Set the default size to 400 pixels wide and 300 pixels high
 master.title("Project_TWA")
 
+#variable = StringVar(master)
+#variable.set("select jobseeker")  # default value
+#variable.trace("w", option_selected)
+
+# Colors and spacing
 master.configure(bg="lightgray")
 
 label_info = Label(master, text="Select one of the categories below!", bg="lightgray", pady=10, font=("Arial", 12))
 label_info.pack()
 
-button1 = Button(master, text="Load JobSeeker Details", bg="blue", fg="white", padx=10, pady=5, font=("Arial", 10, "bold"))
+button1 = Button(master, text="Load JobSeeker Details",
+ bg="blue", fg="white", padx=10, pady=5, font=("Arial", 10, "bold")
+ , command=load_jobseeker)
 button1.pack()
 
 button2 = Button(master, text="Load Employer Details", bg="blue",
@@ -132,4 +154,4 @@ button3 = Button(master, text="Fetch Details", bg="blue", fg="white", padx=10, p
 button3.pack()
 
 
-master.mainloop
+master.mainloop()
