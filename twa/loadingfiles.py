5dfe18d78dc3589bcdafa4046b1eee3b9bd08a12
SBanavethy <swetha.banavethy@slu.edu>
2023-08-11 17:24:31 -0500

    Changed variable

--- twa/loadingfiles.py
+++ twa/loadingfiles.py
@@ -3,31 +3,31 @@ from tkinter import *
 
 def connect_to_database():
     try:
-        con_eb = snowflake.connector.connect(
+        connection = snowflake.connector.connect(
             user='NavyaNelluri',
             password='Navya.c@698',
             account='ujlmjrs-rx06772.snowflakecomputing.com',
             database='PROJECT_TWA',
             schema='PROJECT_TWA_SCHEMA'
         )
-        return con_eb
+        return connection
     except Exception as e:
         print(str(e))
         return None
 
 def fetch_details():
-    con_eb = connect_to_database()
-    if con_eb is not None:
-        cursor = con_eb.cursor()
+    connection = connect_to_database()
+    if connection is not None:
+        cursor = connection.cursor()
         jobSeeker_result = fetch_job_seekers(cursor)
-        JobDetails_result = fetch_job_details(cursor)
+        jobDetails_result = fetch_job_details(cursor)
         cursor.close()
-        con_eb.close()
+        connection.close()
 
-        if jobSeeker_result and JobDetails_result:
+        if jobSeeker_result and jobDetails_result:
             # Display the job seeker and job details
             print("Job Seeker: ", jobSeeker_result)
-            print("Job Details: ", JobDetails_result)
+            print("Job Details: ", jobDetails_result)
         else:
             print("No data found.")
 
